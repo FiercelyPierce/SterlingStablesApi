e4a251b328cbb0e0a8d20999e13eec789bd2daa3
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-10-24 22:54:24 -0700

    Update dependencies and add validation middleware

--- src/routes/goats.js
+++ src/routes/goats.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { userValidationRules, validate } = require('../middleware/validator');
 
 const contactController = require('../controllers/goats');
 
